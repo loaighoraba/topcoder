57dde76d3d7d5dbb81492d700e4ef59c2c5941a1
Loai Ghoraba <loai.ghoraba@gmail.com>
2023-01-15 08:31:44 +0200

    initial solution for puzzle 7: matrix multiplication

---
+++ challenges/puzzle_7_matrix_multiplication.py
@@ -0,0 +1,61 @@
+import numpy as np
+from collections import namedtuple
+
+class MatrixMultiplication:
+    """ 
+        The trivial solution to the matrix multiplication puzzle, of course it 
+        won't pass the tests as it takes too much time. 
+    """
+    def __init__(self, n_rows, max_value):
+        self.n_rows = n_rows
+        self.max_value = max_value
+        self._size = self.n_rows ** 2
+
+    @staticmethod
+    def is_unique(size, matrix):
+        return size == len(set(matrix.flatten()))
+
+    def rec(self, index, matrix):
+        if index == self._size:
+            yield matrix
+            return
+
+        i = index // self.n_rows
+        j = index % self.n_rows
+
+        for value in range(1, self.max_value + 1):
+            matrix[i, j] = value
+            yield from self.rec(index + 1, matrix)
+
+    def run(self):
+        A = np.zeros((self.n_rows, self.n_rows), dtype=int)
+        B = np.zeros((self.n_rows, self.n_rows), dtype=int)
+        min_A, min_B, min_C, min_sum = None, None, None, 10 ** 6
+
+        for sample_A in self.rec(0, A):
+            for sample_B in self.rec(0, B):
+                C = np.matmul(sample_A, sample_B)
+                if self.__class__.is_unique(self._size, C):
+                    c_sum = np.sum(C)
+                    if c_sum < min_sum:
+                        min_sum = c_sum
+                        min_C = C
+                        min_A = np.copy(sample_A)
+                        min_B = np.copy(sample_B)
+
+        Result = namedtuple("Result", ["min_A", "min_B", "min_C", "min_sum"])
+        result = Result(min_A=min_A, min_B=min_B, min_C=min_C, min_sum=min_sum)
+
+        return result
+
+challenge = MatrixMultiplication(n_rows=2, max_value=3)
+result = challenge.run()
+
+min_A = result.min_A
+min_B = result.min_B
+min_C = result.min_C
+min_sum = result.min_sum
+print( f"A:\n{min_A}\n\n"
+        f"B:\n{min_B}\n\n"
+        f"C:\n{min_C}\n\n" 
+        f"Sum:{min_sum}")
